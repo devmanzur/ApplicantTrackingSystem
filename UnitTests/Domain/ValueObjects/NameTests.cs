1427ce9cdd2b77ea04971712e6f7f5c352c90a4b
Manzur Alahi <devmanzur>
2020-12-29 23:35:54 +0600

    renamed Name_create_throws_business_rule_violation_exception_when_name_length_is_less_than_5 -> Name_throws_business_rule_violation_exception_when_name_length_is_less_than_5

--- UnitTests/Domain/ValueObjects/NameTests.cs
+++ UnitTests/Domain/ValueObjects/NameTests.cs
@@ -1,4 +1,4 @@
-using Hahn.ApplicatonProcess.December2020.Domain.Entities;
+using Bogus;
 using Hahn.ApplicatonProcess.December2020.Domain.Exceptions;
 using Hahn.ApplicatonProcess.December2020.Domain.Rules;
 using Hahn.ApplicatonProcess.December2020.Domain.ValueObjects;
@@ -6,14 +6,27 @@ using Xunit;
 
 namespace UnitTests.Domain.ValueObjects
 {
-    public partial class ValueObjectTests
+    public class NameTests
     {
-            
+        private Faker _faker;
+
+        public NameTests()
+        {
+            _faker = new Faker();
+        }
+
+        private string InvalidName()
+        {
+            return _faker.Random.String(1, 4);
+        }
+
+        //Name_create_throws_business_rule_violation_exception_when_name_length_is_less_than_5
+        
         [Fact]
-        public void Name_create_throws_business_rule_violation_exception_when_name_length_is_less_than_5()
+        public void Name_throws_business_rule_violation_exception_when_name_length_is_less_than_5()
         {
             //arrange
-            var invalidName = InvalidString(4);
+            var invalidName = InvalidName();
             var  expectedBusinessRuleViolationException = new BusinessRuleViolationException(new NameLengthMustBeAtLeast5Characters(invalidName));
 
             //act
@@ -23,6 +36,8 @@ namespace UnitTests.Domain.ValueObjects
             Assert.IsType<BusinessRuleViolationException>(userRegistrationException);
             Assert.Equal(expectedBusinessRuleViolationException.Message,userRegistrationException.Message);
         }
+        
+        
         
     }
 }
